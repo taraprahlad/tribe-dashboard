89ec9474d3e08c9bed94e69517121453e2901ea2
Tara Prahlad <tara.prahlad.01@gmail.com>
2026-06-09 12:06:57 -0700

    error handling practice 1

--- learning/oop_practice.py
+++ learning/oop_practice.py
@@ -3,6 +3,10 @@ i'm making a pretty basic to-do list manager just to brush up on oop stuff
 we learned in 15-112 lmao -- i know it's a little unforgiving when it comes to spaces
 but i'm too lazy to fix it and this is just practice anyway so who gives a shit"""
 
+import json
+from pathlib import Path
+SAVE_FILE = Path(__file__).parent / "tasks.json"
+
 class Task:
     # need a description of the task and whether or not it's been completed
     def __init__(self, description):
@@ -15,6 +19,25 @@ class Task:
     def __str__(self): #what it looks like
         checkbox = "x" if self.done else " "
         return f"[{checkbox} {self.description}]"
+    
+    def save(self):
+        #write all tasks to disk as JSON.
+        data = [{"description": t.description, "done": t.done} for t in self.tasks]
+        with open(SAVE_FILE, "w") as f:
+            json.dump(data, f, indent=2)
+
+    def load(self):
+        #load tasks from disk if a save file exists
+        if not SAVE_FILE.exists():
+            return
+        with open(SAVE_FILE, "r") as f:
+            data = json.load(f)
+        self.tasks = [Task(item["description"]) for item in data]
+        for task, item in zip(self.tasks, data):
+            if item["done"]:
+                task.mark_done()
+
+
     
 #our to-do list is a collection of task objects
 class ToDoList:
@@ -47,6 +70,7 @@ class ToDoList:
             
 def main(): #let's run ts
     todo = ToDoList()
+    todo.load()
     while True:
         print("\n--- TO-DO List ---")
         todo.numberdisplay() 
@@ -58,17 +82,24 @@ def main(): #let's run ts
             if description:
                 todo.add(description)
         elif choice == "b":
-            number = int(input("which task number do you want to delete?"))
-            todo.delete(number)
+            try:
+                number = int(input("which task number do you want to delete?"))
+                todo.delete(number)
+            except ValueError:
+                print("gurl type numbers not letters")
         elif choice == "c":
-            number = int(input("what task number did you finish?"))
-            todo.complete(number)
+            try:
+                number = int(input("what task number did you finish?"))
+                todo.complete(number)
+            except ValueError:
+                print("gurl type numbers not letters")
             print("yayyyy nice work!")
         elif choice == "d":
+            todo.save()
             print("see you later :) ")
             break
         else:
-            choice = input("type something coherent please >>> ")
+            choice = input("bitch istg if you don't type something coherent >>> ")
 
 main()
 
